bd5f424f15a93395edb2feb667fa28a05536189b
jackup <yk.58@hotmail.com>
2021-11-22 20:01:48 +0300

    ArrayList ile alakalı Örnek

--- MCD_ArrayListOrnek2/Program.cs
+++ MCD_ArrayListOrnek2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace MCD_ArrayListOrnek2
 {
@@ -10,6 +11,36 @@ namespace MCD_ArrayListOrnek2
     {
         static void Main(string[] args)
         {
+            //ArrayListte tip yok.
+            ArrayList listem = new ArrayList();
+            listem.Add("Faik");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('E');
+            listem.Add(20);
+            listem.Add("Esra");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('K');
+            listem.Add(20);
+            listem.Add("Arif");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('E');
+            listem.Add(20);
+
+            Console.WriteLine("ArrayList oluşturuldu.");
+            Console.WriteLine("Yazdırılıyor.");
+            foreach (var item in listem)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("ArrayList kapasitesi = " + listem.Capacity);
+            listem.Remove("Faik");
+
+            Array.ForEach(listem.ToArray(), x => Console.WriteLine(x));
+
         }
     }
 }
